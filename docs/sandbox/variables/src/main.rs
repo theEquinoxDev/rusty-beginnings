7d34374d6a58837a95705798f64e5c7b98a6a28e
Aditya <adityasuri11ss@gmail.com>
2025-11-16 14:28:04 +0530

    tuples and arrays

---
+++ docs/sandbox/variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+    let a = [1, 2, 3, 4, 5];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+}
