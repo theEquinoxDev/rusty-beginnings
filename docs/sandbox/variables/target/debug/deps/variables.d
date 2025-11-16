69b409b0a92afa19ea129ca9330523bfe1930d56
Aditya <adityasuri11ss@gmail.com>
2025-11-16 14:28:22 +0530

    variables

---
+++ docs/sandbox/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\adity\Documents\ABILITIES\RUST\Docs\sandbox\variables\target\debug\deps\variables.d: src\main.rs
+
+C:\Users\adity\Documents\ABILITIES\RUST\Docs\sandbox\variables\target\debug\deps\variables.exe: src\main.rs
+
+src\main.rs:
---
+++ docs/sandbox/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\adity\Documents\ABILITIES\RUST\Docs\sandbox\variables\target\debug\variables.exe: C:\Users\adity\Documents\ABILITIES\RUST\Docs\sandbox\variables\src\main.rs
