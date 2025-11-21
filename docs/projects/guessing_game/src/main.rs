02ff01f99a28b01e541904a085054316886ed87f
Aditya <adityasuri11ss@gmail.com>
2025-11-21 22:21:50 +0530

    first project

--- docs/projects/guessing_game/src/main.rs
+++ docs/projects/guessing_game/src/main.rs
@@ -1,15 +1,36 @@
+use std::cmp::Ordering;
 use std::io;
 
+use rand::Rng;
+
 fn main() {
-    println!("Welcome to the Guessing Game!"); // println! is a macro that prints text to the console
-    println!("Please enter your guess:");
+    println!("Guess the number!");
 
-    let mut guess = String::new(); // in rust, variables are immutable by default, so we use "mut" to make it mutable..
-    //    The :: syntax in the ::new line indicates that new is an associated function of the String type. An associated function is a function that’s implemented on a type, in this case String. This new function creates a new, empty string
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin() // io is a module and stdin is a function
-        .read_line(&mut guess) // &mut indicates that we are passing a mutable reference to the guess variable. read_line is a method that reads a line from standard input and appends it to the provided string. & indicates that we are passing a reference to the variable rather than the variable itself. references are immutable by default as well.
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
