e1e07eaccb2fa41a597b4e2520c38ce6782f9d9a
Aditya <adityasuri11ss@gmail.com>
2025-11-29 11:29:26 +0530

    for loop

--- sandbox/conditionals_loops/src/main.rs
+++ sandbox/conditionals_loops/src/main.rs
@@ -95,3 +95,19 @@ fn main() {
 
     println!("LIFTOFF!!!");
 }
+
+// 3. For Loop
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+}
+
+fn main() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
